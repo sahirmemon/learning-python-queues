48620e6aefe271f146ea612a8d0cf03a56bcf9d3
Sahir Memon <sahir@starmobileinc.com>
2016-05-24 08:06:34 -0400

    Updated UI

--- static/main.js
+++ static/main.js
@@ -4,6 +4,10 @@
   .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
     function($scope, $log, $http, $timeout) {
 
+    $scope.submitButtonText = 'Submit';
+    $scope.loading = false;
+    $scope.urlerror = false;
+
     $scope.getResults = function() {
       $log.log("test");
 
@@ -11,11 +15,14 @@
       var userInput = $scope.url;
 
       // fire the API request
-      $http.post('/start', {"url": userInput}).
+      $http.post('/start', {'url': userInput}).
         success(function(results) {
           $log.log(results);
           getWordCount(results);
-
+          $scope.wordcounts = null;
+          $scope.loading = true;
+          $scope.submitButtonText = 'Loading...';
+          $scope.urlerror = false;
         }).
         error(function(error) {
           $log.log(error);
@@ -29,19 +36,27 @@
       var poller = function() {
         // fire another request
         $http.get('/results/'+jobID).
-        success(function(data, status, headers, config) {
-          if(status === 202) {
-            $log.log(data, status);
-          } else if (status === 200){
-            $log.log(data);
-            $scope.wordcounts = data;
-            $timeout.cancel(timeout);
-            return false;
-          }
-          // continue to call the poller() function every 2 seconds
-          // until the timeout is cancelled
-          timeout = $timeout(poller, 2000);
-        });
+          success(function(data, status, headers, config) {
+            if(status === 202) {
+              $log.log(data, status);
+            } else if (status === 200){
+              $log.log(data);
+              $scope.loading = false;
+              $scope.submitButtonText = "Submit";
+              $scope.wordcounts = data;
+              $timeout.cancel(timeout);
+              return false;
+            }
+            // continue to call the poller() function every 2 seconds
+            // until the timeout is cancelled
+            timeout = $timeout(poller, 2000);
+          }).
+          error(function(error) {
+            $log.log(error);
+            $scope.loading = false;
+            $scope.submitButtonText = "Submit";
+            $scope.urlerror = true;
+          });
       };
       poller();
     }
